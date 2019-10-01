c87a709b5dcc84cfb9aa2891bca0069e7d3bc3a8
jakuburbanski <55091886+jakuburbanski@users.noreply.github.com>
2019-10-01 10:47:29 +0200

    Add files via upload

---
+++ js/plik.js
@@ -0,0 +1,28 @@
+var name = ("Jakub")
+var surname = ("Urbanski")
+document.write(name+" "+surname)
+
+
+document.write('<br>')
+
+
+function dodawanie(a,b) {
+ return a+b;
+}
+
+function odejmowanie(a,b) {
+ return a-b;
+}
+
+
+function mnozenie(a,b) {
+ return a*b;
+}
+
+document.write('<br>')
+
+document.write("roznica to"+odejmowanie(zmienna1,zmienna2))
+document.write('<br>')
+document.write("dodawanie to"+dodawanie(zmienna1,zmienna2))
+document.write('<br>')
+document.write("mnozenie to"+mnozenie(zmienna1,zmienna2))
