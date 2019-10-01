cba59f9e0b3fdfb0ab3c131d8ae1c7a7b28fca6d
jakuburbanski <55091886+jakuburbanski@users.noreply.github.com>
2019-10-01 09:51:23 +0200

    Add files via upload

---
+++ html/js.js
@@ -0,0 +1,7 @@
+confirm("skrrrrt")
+
+     var gang = "2115"
+     var imie = "Kubi"
+     document.write ("<p style = 'color:violet;'> Gang</p>")
+      document.write(imie)
+      document.write(gang)
